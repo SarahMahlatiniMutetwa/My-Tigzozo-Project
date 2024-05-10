485d1632292e8b4c76231a1456c925df757aa232
SarahMahlatiniMutetwa <sarahmahlatini18@gmail.com>
2024-05-10 14:33:39 +0200

    text

---
+++ script.js
@@ -0,0 +1,15 @@
+
+var navLinks = document.getElementsByClass("mylinks");
+
+
+function layout() {
+    if (backgroundcolor == "#222") {
+        document.getElementsByClass("mylinks").style.color = "white";
+    } 
+}
+
+
+
+
+
+
